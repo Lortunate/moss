6765b306008252b566702b1708786f66d91f9e2b
houvven <houvven@gmail.com>
2025-11-19 02:37:50 +0800

    fix title bar style settings

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -1,12 +1,18 @@
 use tauri::{WebviewUrl, WebviewWindow, WebviewWindowBuilder};
 
 pub fn init_main_window(app: &tauri::App) -> tauri::Result<WebviewWindow> {
-    WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+    let mut builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
         .title("moss")
         .inner_size(1360.0, 860.0)
         .min_inner_size(1200.0, 720.0)
-        .title_bar_style(tauri::TitleBarStyle::Overlay)
-        .hidden_title(true)
-        .visible(false)
-        .build()
+        .visible(false);
+
+    #[cfg(target_os = "macos")]
+    {
+        builder = builder
+            .hidden_title(true)
+            .title_bar_style(tauri::TitleBarStyle::Overlay)
+    }
+
+    builder.build()
 }
